26f104c8530e35da1be6815f2df39954c0ab77c3
Alexander Sattelmaier <alexander@sattelmaier.info>
2025-04-23 18:08:48 +0200

    feat: update environment configuration for API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   googleMapsApiKey: 'GOOGLE_MAPS_API_KEY',
-  travelWarningsApiBase: 'https://your-proxy-server.herokuapp.com/api'
+  travelWarningsApiBase: 'TRAVEL_WARNINGS_API_URL'
 };
