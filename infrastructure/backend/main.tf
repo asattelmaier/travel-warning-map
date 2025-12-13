b9eafa31bc8f48f9e050caad0de83f31e1fbfefe
Alexander Sattelmaier <a.sattelmaier@handelsblattgroup.com>
2025-12-13 08:29:41 +0100

    fix(infrastructure): ensure 512MiB memory for backend to support unthrottled CPU
    Cloud Run requires at least 512MB memory when CPU is 'always allocated' (unthrottled).
    Unthrottled CPU is beneficial for our background cache prefill process.

--- infrastructure/backend/main.tf
+++ infrastructure/backend/main.tf
@@ -58,7 +58,7 @@ resource "google_cloud_run_v2_service" "backend" {
       resources {
         limits = {
           cpu    = "1000m"
-          memory = "256Mi"
+          memory = "512Mi"
         }
       }
     }
