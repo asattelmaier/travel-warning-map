f6f0e9f678d3022b915d3e4380abff836a1d0452
Alexander Sattelmaier <a.sattelmaier@handelsblattgroup.com>
2025-12-13 10:05:10 +0100

    feat: PWA Integration and Build Configuration
    - Added PWA support via @angular/pwa
    
    - Integrated custom icon pack and favicon
    
    - Configured public assets and manifest
    
    - Updated gitignore to exclude environment.prod.ts
    
    - Updated project logo

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  // This placeholder is replaced by the 'replace-api-url' script during build (see package.json)
-  travelWarningsApiBase: 'TRAVEL_WARNINGS_API_URL'
-};
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,16 @@
-import { provideZoneChangeDetection } from "@angular/core";
+import { provideZoneChangeDetection, isDevMode } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { TravelWarningService } from './app/services/travel-warning.service';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideZoneChangeDetection(),provideHttpClient(),
-    TravelWarningService
+    TravelWarningService, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 }).catch(err => console.error(err));
